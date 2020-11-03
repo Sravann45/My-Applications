ea271c2afa06559ecde23c1a8c07aa908c978690
addkumar <addkumar@LIN66005124.corp.capgemini.com>
2020-11-03 09:37:30 +0000

    Commit

---
+++ eureka/src/test/java/sravan/java/EurekaApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package sravan.java;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EurekaApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
