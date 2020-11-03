75bab048c9283746c753b4dcdfabfc2e5579192c
addkumar <addkumar@LIN66005124.corp.capgemini.com>
2020-11-03 08:58:45 +0000

    Commiting the spring MVC

---
+++ SpringMVC/src/main/java/sravan/java/AddController.java
@@ -0,0 +1,18 @@
+package sravan.java;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+	@RequestMapping("/add")
+	public ModelAndView say(@RequestParam("t1") int num1) {
+		ModelAndView mv = new ModelAndView();
+		int k=num1;
+		mv.addObject("result", k);
+		mv.setViewName("result.jsp");
+		return mv;
+	}
+
+}
